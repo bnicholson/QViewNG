54306473b646d3c89a0e757c68013e90e3925e8c
Collyn Brenner <collynbrenner@gmail.com>
2026-01-28 16:57:26 -0600

    missed one file's updates

--- backend/run-tests.sh
+++ backend/run-tests.sh
@@ -3,5 +3,10 @@
 # Load .env.test file
 export $(cat ../.env.test | xargs)
 
+# Load PostgreSQL 16 binaries into PATH (mostly for access to initdb)
+# This was initially added for optimizing speed of integraiton tests by making DB in-memory
+# This may need to be changed based on your PGSQL version and configuration.
+export PATH="/usr/lib/postgresql/16/bin:$PATH"
+
 # Run tests
 cargo test -- --test-threads=1  # uses a single test DB synchronously (for now)
