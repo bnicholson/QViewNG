36482d870519cdcd791337b5e3beeedad71b8a39
Collyn Brenner <collynbrenner@gmail.com>
2026-02-05 17:27:01 -0600

    added passing integration test for get monitor by ID endpoint

--- backend/tests/monitor_test.rs
+++ backend/tests/monitor_test.rs
@@ -104,40 +104,41 @@ async fn get_all_works() {
     assert_ne!(monitor_2_interest_idx, 10);
 }
 
-// #[actix_web::test]
-// async fn get_by_id_works() {
+#[actix_web::test]
+async fn get_by_id_works() {
 
-//     // Arrange:
+    // Arrange:
     
-//     clean_database();
-//     let db = Database::new(TEST_DB_URL);
-//     let mut conn = db.get_connection().expect("Failed to get connection.");
+    clean_database();
+    let db = Database::new(TEST_DB_URL);
+    let mut conn = db.get_connection().expect("Failed to get connection.");
     
-//     let monitor = 
-//         fixtures::monitors::arrange_get_monitor_by_id_works_integration_test(&mut conn);
+    let monitor = 
+        fixtures::monitors::arrange_get_monitor_by_id_works_integration_test(&mut conn);
 
-//     let app = test::init_service(
-//         App::new()
-//             .app_data(web::Data::new(db))
-//             .configure(configure_routes)
-//     ).await;
+    let app = test::init_service(
+        App::new()
+            .app_data(web::Data::new(db))
+            .configure(configure_routes)
+    ).await;
 
-//     let uri = format!("/api/equipment/monitors/{}", &monitor.equipmentid);
-//     println!("Monitors Get by ID URI: {}", &uri);
-//     let req = test::TestRequest::get()
-//         .uri(uri.as_str())
-//         .to_request();
+    let uri = format!("/api/equipment/monitors/{}", &monitor.equipmentid);
+    println!("Monitors Get by ID URI: {}", &uri);
+    let req = test::TestRequest::get()
+        .uri(uri.as_str())
+        .to_request();
 
-//     // Act:
+    // Act:
     
-//     let resp = test::call_service(&app, req).await;
-//     assert_eq!(resp.status(), StatusCode::OK);
+    let resp = test::call_service(&app, req).await;
+    assert_eq!(resp.status(), StatusCode::OK);
 
-//     // Assert:
+    // Assert:
     
-//     let resp_monitor: Monitor = test::read_body_json(resp).await;
-//     assert_eq!(resp_monitor.serial_number, monitor.serial_number);
-// }
+    let resp_monitor: Monitor = test::read_body_json(resp).await;
+    assert_eq!(resp_monitor.id, monitor.id);
+    assert_eq!(resp_monitor.brand, monitor.brand);
+}
 
 // #[actix_web::test]
 // async fn update_works() {
