0aa7e62f8f1c0aa522d0a2a3d285310b9af7a309
Collyn Brenner <collynbrenner@gmail.com>
2026-02-05 21:33:13 -0600

    added passing integration test for delete microphonerecorder endpoint

--- backend/tests/fixtures/microphonerecorders.rs
+++ backend/tests/fixtures/microphonerecorders.rs
@@ -84,21 +84,20 @@ pub fn arrange_update_works_integration_test(db: &mut database::Connection) -> M
         .unwrap()
 }
 
-// pub fn arrange_delete_works_integration_test(db: &mut database::Connection) -> MicrophoneRecorder {
-//     let user = UserBuilder::new_default("User 1")
-//         .set_hash_password("SOmeTHinGSeCUre!23")
-//         .build_and_insert(db)
-//         .unwrap();
-//     let equipment_set = EquipmentSetBuilder::new_default(user.id)
-//         .set_is_active(true)
-//         .set_is_default(true)
-//         .set_description(Some("This is a test equipment set.".to_string()))
-//         .build_and_insert(db)
-//         .unwrap();
-//     MicrophoneRecorderBuilder::new_default(equipment_set.id)
-//         .set_type_(Some("External".to_string()))
-//         .set_brand(Some("Brand H".to_string()))
-//         .set_misc_note(Some("Test microphonerecorder for delete.".to_string()))
-//         .build_and_insert(db)
-//         .unwrap()
-// }
+pub fn arrange_delete_works_integration_test(db: &mut database::Connection) -> MicrophoneRecorder {
+    let user = UserBuilder::new_default("User 1")
+        .set_hash_password("SOmeTHinGSeCUre!23")
+        .build_and_insert(db)
+        .unwrap();
+    let equipment_set = EquipmentSetBuilder::new_default(user.id)
+        .set_is_active(true)
+        .set_is_default(true)
+        .set_description(Some("This is a test equipment set.".to_string()))
+        .build_and_insert(db)
+        .unwrap();
+    MicrophoneRecorderBuilder::new_default(equipment_set.id)
+        .set_type_(Some("External".to_string()))
+        .set_misc_note(Some("Test microphonerecorder for delete.".to_string()))
+        .build_and_insert(db)
+        .unwrap()
+}
